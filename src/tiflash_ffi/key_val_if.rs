1d0427ac271fc33359fef838dc072e88f3e99820
Tong Zhigao <tongzhigao@pingcap.com>
2020-04-28 17:45:54 +0800

    update
    Signed-off-by: Tong Zhigao <tongzhigao@pingcap.com>

--- src/tiflash_ffi/key_val_if.rs
+++ src/tiflash_ffi/key_val_if.rs
@@ -1,6 +1,5 @@
 use crate::tiflash_ffi::invoke::{get_tiflash_server_helper, BaseBuffView};
-use engine::rocks::Writable;
-use engine::{Peekable, CF_DEFAULT, CF_LOCK, CF_RAFT, CF_TIFLASH, CF_WRITE, DB};
+use engine::{Peekable, WriteOptions, CF_DEFAULT, CF_LOCK, CF_RAFT, CF_TIFLASH, CF_WRITE, DB};
 use std::sync::Arc;
 
 enum DBGetValueCfState {
@@ -30,15 +29,23 @@ pub extern "C" fn ffi_handle_put_tiflash_cf(
     proxy_ptr: TiFlashRaftProxyPtr,
     key_: BaseBuffView,
     value_: BaseBuffView,
+    sync: u8,
 ) -> i32 {
     unsafe {
         let key = key_.to_slice();
         let value = value_.to_slice();
 
         let handle = (*proxy_ptr).storage.cf_handle(CF_TIFLASH).unwrap();
+
+        let option = {
+            let mut option = WriteOptions::new();
+            option.set_sync(sync != 0);
+            option
+        };
+
         match (*proxy_ptr)
             .storage
-            .put_cf(handle, key.as_ref(), value.as_ref())
+            .put_cf_opt(handle, key.as_ref(), value.as_ref(), &option)
         {
             Ok(_) => DBGetValueCfState::OK.into(),
             _ => DBGetValueCfState::Error.into(),
@@ -92,7 +99,8 @@ type TiFlashRaftProxyPtr = *const TiFlashRaftProxy;
 #[repr(C)]
 pub struct TiFlashRaftProxyHelper {
     proxy_ptr: TiFlashRaftProxyPtr,
-    handle_put_tiflash_cf: extern "C" fn(TiFlashRaftProxyPtr, BaseBuffView, BaseBuffView) -> i32,
+    handle_put_tiflash_cf:
+        extern "C" fn(TiFlashRaftProxyPtr, BaseBuffView, BaseBuffView, u8) -> i32,
     handle_get_value_cf: extern "C" fn(TiFlashRaftProxyPtr, BaseBuffView, u8) -> DBGetValueCfRes,
 }
 
