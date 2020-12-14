2d32433471a838e01d79c924b0ccac42d0f3005f
Tong Zhigao <tongzhigao@pingcap.com>
2020-12-14 18:20:22 +0800

    Fix compile: do not enable grpcio/openssl-vendored on macos
    Signed-off-by: Tong Zhigao <tongzhigao@pingcap.com>

--- release.sh
+++ release.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+set -e
 export BUILD_TYPE=release
 export PROXY_PROFILE=release
 if [[ $(uname -s) == "Darwin" ]]; then
   echo "Kernel is Darwin, change build type to debug"
   unset BUILD_TYPE
   export PROXY_PROFILE=debug
+  brew --prefix openssl
+#  export OPENSSL_ROOT_DIR=$(brew --prefix openssl)
+#  export OPENSSL_LIB_DIR=$(brew --prefix openssl)"/lib"
+#  export OPENSSL_INCLUDE_DIR=$(brew --prefix openssl)"/include"
+#  export OPENSSL_NO_VENDOR=1
   make build_by_type
   mkdir -p target/release
   cp target/debug/libtiflash_proxy.dylib target/release/libtiflash_proxy.dylib
